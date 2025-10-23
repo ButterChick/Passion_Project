0933d22317e48f332bf086e69db5ebaf4642e272
ButterChick <dhruvphooli@gmail.com>
2025-10-24 00:10:13 +0530

    Make a table

--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Table from "../compents/Table";
 
 const Patients = () => {
     return (
         <div>
             <h1>Table of all Exisiting Patients(Heading)</h1>
-            <div> Actual Table of all Patients</div>
+            <Table/>
             <p>In the Table there will be 2 notes, One to add Daily Notes and other to Get the discharge summary</p>
         </div>
     );
