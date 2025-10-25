3c1a3d68d32466f6538cd1f1b17a22c55030fd77
ButterChick <dhruvphooli@gmail.com>
2025-10-25 11:42:40 +0530

    Made the table, it takes data from an array for now, Ill get it hooked to a backend so that it takes data from there only

--- src/compents/Table.jsx
+++ src/compents/Table.jsx
@@ -1,7 +1,60 @@
 import React from "react";
 
 const Table = () => {
-    return <div>Table</div>;
+    const patients = [
+        { name: "Dhruv", status: "Stable", ward: "A1" },
+        { name: "Tayagi", status: "Critical", ward: "B2" },
+    ];
+    return (
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Patient Name
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Status
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Ward
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Daily Notes
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Discharge Summary
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {patients.map((patient, index) => (
+                        <tr key ={index} 
+                        className="odd:bg-white odd:dark:bg-gray-900 
+                        even:bg-gray-50 even:dark:bg-gray-800 dark:border-gray-700 border-gray-200 ">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {patient.name}
+                            </th>
+                            <td className="px-6 py-4">{patient.status}</td>
+                            <td className="px-6 py-4">{patient.ward}</td>
+                            <td>
+                                <a href="#" 
+                                className="px-6 py-4 font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                                    Daily Notes
+                                </a>
+                            </td>
+                            <td>
+                                <a href="#" 
+                                className="px-6 py-4 font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                                    Discharge Summary
+                                </a>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default Table;
--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -5,7 +5,9 @@ const Patients = () => {
     return (
         <div>
             <h1>Table of all Exisiting Patients(Heading)</h1>
-            <Table/>
+            <div className="px-5 py-5">
+                <Table/>
+            </div>
             <p>In the Table there will be 2 notes, One to add Daily Notes and other to Get the discharge summary</p>
         </div>
     );
